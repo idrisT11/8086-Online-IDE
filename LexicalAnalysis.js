11df5e86040ad74cdaf7119ac23b5cef06c9ddc3
idrisT11 <64688223+idrisT11@users.noreply.github.com>
2021-04-08 18:53:44 +0100

    Update LexicalAnalysis.js

--- LexicalAnalysis.js
+++ LexicalAnalysis.js
@@ -1,1 +1,49 @@
 //MOH
+   const wordRegisters = ['AX', 'BX', 'CX', 'DX', 'CS', 'DS', 'ES', 'SS', 'DI', 'SI', 'SP', 'BP', 'IP'];
+   const byteRegisters = ['AH', 'AL', 'BH', 'BL', 'CH', 'CL', 'DL', 'DH'];
+   const segmentRegisters = ['CS', 'DS', 'ES', 'SS'];
+   const instructions=["ENDM","EQU","DEFINE","ORG","ENDP","MOV","PUSH", "POP", "XCHG","LEA","LAHF","SAHF","PUSHF","POPF" ,"ADD","ADC","DEC" ,"INC","AAA","SUB","SSB" ,"NEG" ,"CMP" ,"MUL","IMUL","DIV","IDIV","CBW","CWD","NOT","SHL","SAL","SHR", "SAR","ROL","ROR", "RCL","RCR","AND","TEST","OR","XOR","REP","MOVSB", "CMPSB", "SCASB","LODSB","STOSB","MOVSW", "CMPSW", "SCASW","LODSW","STOSW","CALL"]
+    
+"use strict";
+
+class LexicalAnalysis{
+    lexical={};
+     //return the firstKlma word from  a string 
+    extractfirstAwel(str){
+         return str.trim().match(/(?<=\s*)[\w|^\:]:/);
+    }
+
+    execute(){
+        firstKlma = extractfirstAwel(str);
+        if ( firstKlma[ firstKlma.length() - 1]==':'){
+            if (legalName(firstKlma)){
+                this.lexical.label=firstKlma;//delete the ":"  
+                str = str.replace(firstKlma,"").trim();
+                firstKlma = extractfirstAwel(str);
+            }
+            else {
+
+                this.lexical.good=false;
+                this.lexical.message="ERROR : Illegel expression ";
+                console.log("errue : label false ");
+            }
+
+        }
+         if (second){}; // four cases
+
+    }
+
+    //check if the name is legal
+    legalName(str){
+        str = str.toUpperCase().trim();
+        if ((/\W/.test(str))||(/^\d/.test(str)) || instructions.includes(str) || wordRegisters.includes(str) || byteRegisters.includes(str)) 
+            return false;
+        return true;
+    }
+    testVariable(str){
+      
+    } 
+}
+
+let lx = new LexicalAnalysis();
+console.log(lx.legalName("EU"));
