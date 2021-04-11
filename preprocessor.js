48549715a2a40515471f07237ae1b39b2d1ec47a
Scorpion197 <doudou.gaw@gmail.com>
2021-04-11 21:42:18 +0200

    Added preprocessing methods

---
+++ preprocessor.js
@@ -0,0 +1,101 @@
+class PreProcessor {
+   
+    constructor() {
+
+        this.variables = [];
+        this.labels = [];
+
+    }
+    static executePostLexical(lexicalView) {
+
+
+    }
+
+    getVariables(objectsArray) {
+
+        // looping through each line and extract variable names and labels
+
+        var variableObjects = {
+
+            line : null,
+            size: null, 
+            varName: "", 
+
+        }
+
+        for (let i = 0; i < objectsArray.length; i++) {
+
+            if (objectsArray[i].expressionType == "VAR" && objectsArray[i].variableName != null) {
+
+                variableObjects.line = i; 
+                variableObjects.varName = objectsArray[i].variableName; 
+
+                if(/DB/.test(objectsArray[i].variableClass))
+
+                    variableObjects.size = "BYTE";
+
+                else if(/DW/.test(objectsArray[i].variableClass))
+
+                    variableObjects.size = "WORD";
+
+                this.variables.push(variableObjects);
+
+            }
+
+            
+        }
+    }
+    static executeDefine(code, obj) {
+
+        // code is the whole code written by the user
+        // obj is an object
+
+        // convert the code into an array of instructions
+        var instArray = code.split('\n');
+        
+        for (var i = 0; i < instArray.length; i++) {
+
+            if (instArray[i] != "" || instArray[i] != " ") {
+
+                if (/DEFINE/.test(instArray[i])) {
+
+                    // first value after the DEFINE keyword 
+                    var firstValue = obj.operands[0]; 
+
+                    // second value after the DEFINE with which we will replace it
+                    var secondValue = obj.operands[1];  
+
+                }
+
+            }
+        }
+
+        // get the instructions which doesn't contain DEFINE 
+        var opcodes = instArray.filter(inst => !/DEFINE/.test(inst));
+
+        // replacing in source code 
+        opcodes = opcodes.map((item) => {if (item.indexOf(firstValue) != -1) return item.replace(firstValue, secondValue); else return item});
+
+        return opcodes.join('\n');
+    }
+
+}
+
+
+// testing 
+// var testObj = new PreProcessor(); 
+var op = {
+    operands : ["A", "10"],
+}
+
+
+var sub = document.getElementById("sub"); 
+    text = document.getElementById("code");
+    code = "";
+    instArray = [];
+
+sub.onclick = function() {
+
+    code = text.value; 
+    console.log(PreProcessor.executeDefine(code, op));
+}
