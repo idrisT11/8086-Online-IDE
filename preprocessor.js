97d3c166608a9d22d2d4b1cd010b8125733476b6
Gaouaoui Kamel <doudou.gaw@gmail.com>
2021-04-13 14:18:44 +0200

    Finished manageVariables function
    the "manageVariables" function is finished and works without errors ( i hope so)

--- preprocessor.js
+++ preprocessor.js
@@ -6,7 +6,7 @@ class PreProcessor {
         this.labels = [];
         this.good = true; 
         this.message = ""; 
-        this.lineError = null;
+        this.errorLine = null;
 
     }
 
@@ -15,27 +15,51 @@ class PreProcessor {
 
     }
 
+    _checkVariable(varName) {
+
+        // Check if varName exists in this.variables and in this.labels
+        
+        // check in this.variables
+        for (var i = 0; i < this.variables; i++) {
+
+            if (this.variables[i].name == varName)
+
+                // it exists
+                return true;
+        }
+
+        for (var i = 0; i < this.labels; i++) {
+
+            if (this.labels[i].name == varName)
+                
+                return true;
+        }
+
+        return false;
+
+    }
     getVariables(objectsArray) {
 
         // looping through each line and extract variable names and labels
 
-        var variablesObject = {
-
-            line : null,
-            size: null, 
-            varName: null, 
-
-        }
-
-        var labelsObject = {
-
-            line: null, 
-            labelName : null,
-        }
 
         for (var i = 0; i < objectsArray.length; i++) {
 
-            if (objectsArray[i].expressionType == "VAR" && objectsArray[i].variableName != null) {
+            var variablesObject = {
+
+                line : null,
+                size: null, 
+                varName: null, 
+    
+            }
+
+            var labelsObject = {
+
+                line: null, 
+                labelName : null,
+            }
+
+            if (objectsArray[i].expressionType == "VAR" && objectsArray[i].variableName != null && !this._checkVariable(objectsArray[i].variableName)) {
 
                 variablesObject.line = i; 
                 variablesObject.varName = objectsArray[i].variableName.toUpperCase(); 
@@ -48,8 +72,8 @@ class PreProcessor {
 
                     variablesObject.size = "WORD";
 
-                //this.variables.push(variablesObject);
-                
+                this.variables.push(variablesObject);
+               
             }
 
                 
@@ -63,9 +87,23 @@ class PreProcessor {
 
             }
 
+
         }
 
-        console.log(this.variables);
+    }
+
+    _findInLabels(varName) {
+
+        // function which looks for a variable in labels array 
+
+        for(var i = 0; i < this.labels.length; i++) {
+
+            if (this.labels[i].labelName == varName.toUpperCase())
+
+                return true;
+        }
+
+        return false;
     }
 
     manageVariables(objectsArray) {
@@ -77,77 +115,62 @@ class PreProcessor {
 
             for (var j = 0; j < objectsArray[i].operands.length; j++) {
 
-                found = false;
 
                 if (/VAR/.test(objectsArray[i].operands[j].type)) {
 
+                    found = false;
 
                     for (var k = 0; k < this.variables.length; k++) {
 
-                        if (this.variables[k].varName == objectsArray[i].operands[j].name) {
+
+                        if (this.variables[k].varName.toUpperCase() == objectsArray[i].operands[j].name.toUpperCase()) {
+
+                            /*console.log("OBJECT NUM:", i); 
+                            console.log("THIS.VARIABLES: ", this.variables[k].varName);
+                            console.log("OBJECTS VAR NAME: ", objectsArray[i].operands[j].name);
+                            console.log("======================");*/
 
                             found = true; 
 
                             if (/BYTE/.test(this.variables[k].size)) 
-                                objectsArray[i].operands[j].name = "VAR 8";
+                                objectsArray[i].operands[j].type = "VAR8";
 
                             else if (/WORD/.test(this.variables[k].size))
-                                objectsArray[i].operands[j].name = "VAR 16";
+                                objectsArray[i].operands[j].type = "VAR16";
 
                             break;
+                            
                         }
 
 
                     }
 
                     if (!found) {
+                            
+                        let varName = objectsArray[i].operands[j].name; 
+                        
+                        if (this._findInLabels(varName))
+                            objectsArray[i].operands[j].type = "LBL"; 
 
-                        this.good = false;
-                        this.message = "Error: Variable doesn't exist"; 
-                        this.lineError = i; 
+                        else {
 
-                        return -1;
-                    }
+                            this.good = false;
+                            this.errorLine = i; 
+                            this.message = "Variable/Label doesn't exist"; 
 
+                            return -1;
+                        }
+
+                        }
                     
                 }
             }
 
         }
-
-        // looking for a specific variable in labels array 
-
-        for (var i = 0; i < objectsArray.length; i++) {
-
-            for (var j = 0; j < objectsArray[i].operands.length; j++) {
-
-                found = false; 
-
-                if (/VAR/.test(objectsArray[i].operands[j].type)) {
-
-                    for (var k = 0; k < this.variables; k++) {
-
-                        if (this.variables[k].varName == objectsArray[i].operands[j].name) {
-
-                            found = true; 
-
-                            objectsArray[i].operands[j].name = "LBL";
-                        }
-                    }
-
-                    if (!found) {
-
-                        this.good = false;
-                        this.message = "Error: variable doesn't exist"; 
-                        this.lineError = i; 
-
-                        return -1;
-
-                    }
-                }
-            }
-        }
+        
+        return 0;
     }
+
 
 
     static executeDefine(code, obj) {
@@ -196,26 +219,48 @@ var objectsArray = [{
     expressionType: "VAR",
     variableName: "X", 
     variableClass: "DB", 
-    label: "label 1",    
+    label: "label1",  
+    operands:[{
+        name: "X", 
+        type: 'VAR',
+    },
+    {name: "X",
+    type: 'VAR',},
+    {name: "Y", 
+    type: "VAR"},],
 
+},
+{
+    expressionType:"VAR", 
+    variableName: "A", 
+    variableClass: "DW", 
+    label: "label2",
+    operands:[{
+        name: "label1", 
+        type: "VAR",
+    }],
 },
 {
     expressionType:"VAR", 
     variableName: "Y", 
-    variableClass: "DW", 
-    label: "label 2",
-},
-{
-    expressionType:"VAR", 
-    variableName: "Z", 
     variableClass: "DB", 
-    label: "label 3",
+    label: "label3",
+    operands:[{
+        name: "Y", 
+        type: "VAR",
+    },
+    {name: "A",
+    type: "VAR",},],
 },];
 
 var testObj = new PreProcessor(); 
 testObj.getVariables(objectsArray);
+console.log(testObj.variables);
+console.log(testObj.manageVariables(objectsArray));
 
-// more testing is needed
+//console.log(testObj.message);
+//console.log(testObj.labels);
+//console.log(testObj.variables);
 /*var sub = document.getElementById("sub"); 
     text = document.getElementById("code");
     code = "";
