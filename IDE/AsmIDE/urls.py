e4d53cf7fa70ae55a45fba9856422a8f071774ca
Scorpion197 <doudou.gaw@gmail.com>
2021-06-01 04:01:02 +0200

    Updated some styling

--- IDE/AsmIDE/urls.py
+++ IDE/AsmIDE/urls.py
@@ -9,9 +9,8 @@ urlpatterns = [
     path('send_test/', views.send_test_page, name='send_test_page'),
     path('admin_login/', views.admin_login, name="admin_login"),
     path('dashboard/',views.dashboard, name='dashboard'),
-
     path('dashboard/create_test', views.create_test, name='create_test'),
     path('dashboard/alltests', views.display_tests, name='see_all_tests'),
     path('dashboard/tests/<int:test_id>', views.display_students, name='display_students'),
-    path('dashboard/studens/<int:student_id>', views.display_student_submission, name="view_submission"),
+    path('dashboard/students/<int:student_id>', views.display_student_submission, name="view_submission"),
 ]
